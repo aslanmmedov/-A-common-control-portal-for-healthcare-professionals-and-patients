b04d27ce38db12d4eea8c20e32167f60811f549a
aslanmmedov <aslanzm-azmp202@code.edu.az>
2025-02-16 09:18:13 -0800

    working on home page

--- client/src/Pages/Client/Home/index.jsx
+++ client/src/Pages/Client/Home/index.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import "./index.scss";
+import { FaArrowRightLong } from "react-icons/fa6";
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <main>
+        <section id="heroBanner">
+          <div className="container">
+            <div className="herobanner">
+                <div className="heroText">
+                <h1>Poliklinikaların elektron nəzarət və idarəetmə portalı</h1>
+                <p>E-Poliklinika-nın fəaliyyətinin əsas məqsədi tibb müəssisələrində tibbi xidmətlərin təşkilini təmin etməkdən və tibbi xidmətlərin keyfiyyətinin yüksəldilməsi üçün tədbirlər görməkdən ibarətdir.</p>
+                <button>Ətraflı məlumat<span><FaArrowRightLong /></span></button>
+                </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+};
 
-export default Home
+export default Home;
